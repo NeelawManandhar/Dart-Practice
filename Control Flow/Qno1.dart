1230dc35d3613cefa08d575cb3b5f73a1f825e19
unknown <neelawmanandhar@gmail.com>
2026-01-14 12:10:34 +0545

    Add in Control Flow - 2 questions

---
+++ Control Flow/Qno1.dart
@@ -0,0 +1,13 @@
+/* 1. Write a program that checks if a number is positive, negative, or zero. */
+void main(){
+  int num = 23;
+  if(num>0){
+    print("It is a positive number.");
+  }
+  else if(num < 0){
+    print("It is a negetive number.");
+  }
+  else{
+    print("It is exactly Zero");
+  }
+}
---
+++ Control Flow/Qno2.dart
@@ -0,0 +1,10 @@
+/* 2. Write a program that checks if a person is eligible to vote (age ≥ 18). */
+void main(){
+  int age = 23;
+  if(age>= 18){
+    print("You are eligible to vote!");
+  }
+  else{
+    print("You are not eligible to vote!");
+  }
+}
