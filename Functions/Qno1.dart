79b67e5396d2ae9012e848a8311dc4a98a78a9e6
unknown <neelawmanandhar@gmail.com>
2026-01-16 11:15:45 +0545

    Add Functions - 2  questions

---
+++ Functions/Qno1.dart
@@ -0,0 +1,8 @@
+/* 1. Write a function greetUser() that prints "Hello, Neelaw!". */
+void greetUser(){
+  print("Hello, Neelaw!");
+}
+void main(){
+  //Function call
+  greetUser();
+}
---
+++ Functions/Qno2.dart
@@ -0,0 +1,9 @@
+/* 2. Write a function multiply(int a, int b) that multiplies two numbers and prints the result. */
+void multiply(int a , int b){
+  print("$a X $b = ${a*b}");
+
+}
+
+void main(){
+  multiply(2, 4);
+}
