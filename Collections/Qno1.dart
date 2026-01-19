05e50bfd0b4313798b77546d3798324a7b353586
unknown <neelawmanandhar@gmail.com>
2026-01-19 21:25:34 +0545

    Add Collections - Question

--- Collections/Qno1.dart
+++ Collections/Qno1.dart
@@ -1,7 +1,5 @@
 /* 1. Create a list of 5 favorite movies and print them one by one using a loop. */
 void main(){
   List<String> movies = ["Inception", "Home Alone", "Shutter Island", "Fast and Furious", "Race" ];
-  for(int i =0;i<=4;i++){
-    print(movies[i]);
-  }
+print(movies);
 }
---
+++ Collections/Qno4.dart
@@ -0,0 +1,7 @@
+/* 4. Create a set of numbers {1, 2, 3, 4, 4, 5} and print it. What happens to the duplicate? */
+void main(){
+  // ignore: equal_elements_in_set
+  Set<int> numbers ={1,2,3,4,4,5};
+  print(numbers);
+  //The duplicate valuse is not printed.
+}
